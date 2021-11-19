58511c08b87985e30e4e40e3cbca9c4635004dcd
oichii <i.hawk@yandex.ru>
2021-11-19 10:18:24 +0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import fetch from "node-fetch";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      curUrl: "",
+      loaded: false,
+      rememberedSchemes: [],
+    };
+    this.schemeUrls = [];
+    this.curSchema = 0;
+  }
+
+  shuffleDeck() {
+    console.log("shuffling...");
+    this.schemeUrls = this.schemeUrls.sort((a, b) => 0.5 - Math.random());
+    this.curSchema = 0;
+    this.setState({ curUrl: this.schemeUrls[0] });
+  }
+
+  nextCard() {
+    this.curSchema++;
+    if (this.curSchema === this.schemeUrls.length) this.curSchema = 0;
+    this.setState({ curUrl: this.schemeUrls[this.curSchema] });
+  }
+
+  componentDidMount() {
+    fetch("https://api.magicthegathering.io/v1/cards?type=scheme").then(
+      (response) => {
+        response.json().then((info) => {
+          info.cards.forEach((card) => {
+            if (card.imageUrl) this.schemeUrls.push(card.imageUrl);
+          });
+          console.log("fetched");
+          this.shuffleDeck();
+          this.setState({ loaded: true });
+        });
+      }
+    );
+  }
+
+  rememberCurrentScheme() {
+    if (!this.state.rememberedSchemes.includes(this.state.curUrl))
+      this.setState({
+        rememberedSchemes: [this.state.curUrl, ...this.state.rememberedSchemes],
+      });
+  }
+
+  removeOngoingScheme(index) {
+    this.setState({
+      rememberedSchemes: this.state.rememberedSchemes.filter(
+        (url, i) => index !== i
+      ),
+    });
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.loaded ? (
+          <div>
+            <div>
+              <button
+                onClick={this.shuffleDeck.bind(this)}
+                className="deck-button"
+              >
+                Shuffle
+              </button>
+              <button
+                onClick={this.nextCard.bind(this)}
+                className="deck-button"
+              >
+                Next
+              </button>
+            </div>
+            <img src={this.state.curUrl} alt="" className="schema-image" />
+            <br />
+            <button onClick={this.rememberCurrentScheme.bind(this)}>
+              Remember
+            </button>
+            <br />
+            {this.state.rememberedSchemes.map((url, i) => {
+              return (
+                <img
+                  src={url}
+                  alt=""
+                  key={url}
+                  onClick={() => this.removeOngoingScheme(i)}
+                  className="schema-image"
+                ></img>
+              );
+            })}
+          </div>
+        ) : (
+          <div>
+            <h2>Loading schemes...</h2>
+          </div>
+        )}
+      </>
+    );
+  }
 }
 
 export default App;
